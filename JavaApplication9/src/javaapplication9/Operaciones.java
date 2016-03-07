504fcd3dfa0efb92eb0d4e5fb0a8c67e8e6d49da
mcruz <mcruz@mcruz-hp>
2016-03-07 01:40:49 +0100

    versión 1

---
+++ JavaApplication9/src/javaapplication9/Operaciones.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author mcruz
+ */
+public class Operaciones {
+    
+    public static int suma(int a, int b){
+        return a+b;
+    }
+    
+    public static int resta(int a, int b){
+        return a-b;
+    }
+    
+    public static int producto(int a, int b){
+        return a*b;
+    }
+    
+    public static int division(int a, int b){
+        return a/b;
+    }
+}
